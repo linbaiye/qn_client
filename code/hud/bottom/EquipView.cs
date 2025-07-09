b8d1ba31684de1044ab1124133e9221763098215
Kyle Lin <kyle.lin@wirelesscar.com>
2025-07-09 17:45:32 +0800

    fixes.

--- code/hud/bottom/EquipView.cs
+++ code/hud/bottom/EquipView.cs
@@ -1,4 +1,5 @@
 using Godot;
+using NLog;
 using QnClient.code.message;
 using QnClient.code.player;
 using QnClient.code.sprite;
@@ -13,25 +14,33 @@ public partial class EquipView : NinePatchRect
     
     private const int AvatarIndex = 57;
 
-    private TextureRect _body;
-    private TextureRect _weapon;
+    private TextureButton _body;
+    private TextureButton _weapon;
 
     private Vector2 _bodyOffset;
+
+    private static readonly ILogger Logger = LogManager.GetCurrentClassLogger();
 
 
     public override void _Ready()
     {
-        _body = GetNode<TextureRect>("Body");
-        _weapon = GetNode<TextureRect>("Weapon");
+        _body = GetNode<TextureButton>("Body");
+        _weapon = GetNode<TextureButton>("Weapon");
+        _weapon.Pressed += () => OnPressed(EquipmentType.Weapon);
     }
-    
+
+
+    private void OnPressed(EquipmentType type)
+    {
+        Logger.Debug("{} pressed.", type);
+    }
 
     public void OnCharacterJoined(JoinRealmMessage message)
     {
         _male = message.Male;
         var textures = SpriteLoader.Load(_male ? "N00" : "A00");
         var offsetTexture = textures[AvatarIndex];
-        _body.Texture = offsetTexture.Texture;
+        _body.TextureNormal = offsetTexture.Texture;
         _body.Position = new Vector2((Size.X - offsetTexture.OriginalSize.X) / 2, (Size.Y - offsetTexture.OriginalSize.Y) / 2);
         _bodyOffset = _body.Position - offsetTexture.Offset;
         foreach (var equip in message.Equipments)
@@ -40,7 +49,7 @@ public partial class EquipView : NinePatchRect
             {
                 textures = SpriteLoader.Load(equip.SpritePrefix  + "0");
                 offsetTexture = textures[AvatarIndex];
-                _weapon.Texture = offsetTexture.Texture;
+                _weapon.TextureNormal = offsetTexture.Texture;
                 _weapon.Position = _bodyOffset + offsetTexture.Offset;
             }
         }
