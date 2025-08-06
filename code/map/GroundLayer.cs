1a883e92a6eb34be7141ec7cace23f6f223fa824
Kyle <linbaiye@gmail.com>
2025-08-06 22:02:57 +0800

    Backup.

--- code/map/GroundLayer.cs
+++ code/map/GroundLayer.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using Godot;
 
 namespace QnClient.code.map;
@@ -22,5 +23,20 @@ public partial class GroundLayer : AbstractGroundLayer
                 SetCell(new Vector2I(x, y), tileSourceId, new Vector2I(cell.TileNumber, 0));
             }
         });
+    }
+
+    public void DumpPattern(AtzMapFileParser atzMapFileParser)
+    {
+        ISet<int> sets = new HashSet<int>();
+        atzMapFileParser.ForeachCell(new Vector2I(30, 30), new Vector2I(50, 50), (cell, x, y) =>
+        {
+            GD.Print(new Vector2I(x, y) + ":" + cell.TileNumber);
+            sets.Add(cell.TileNumber);
+        });
+        var orderedEnumerable = sets.OrderBy(i => i);
+        foreach (var i in orderedEnumerable)
+        {
+        GD.Print(i);
+        }
     }
 }
