ff645625c7af69a4e1ad5952d968d7ef39f94681
Kyle <linbaiye@gmail.com>
2025-08-12 00:43:29 +0800

    add files.

---
+++ code/hud/bottom/PrivateChatManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace QnClient.code.hud.bottom;
+
+public class PrivateChatManager
+{
+    private readonly List<string> _privateChatMessages = new();
+    
+    
+    
+}
